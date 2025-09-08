29638ab343ee332a78b1b705479c7cdc860ed26b
Eric-Cortez <ecrcortez@gmail.com>
2025-09-07 21:21:11 -0700

    fix: remove duplicate back to home link

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import Layout from '../../components/layout';
+import styles from '../../styles/FirstPost.module.css';
 
 export default function FirstPost() {
     return (
@@ -8,10 +8,7 @@ export default function FirstPost() {
             <Head>
                 <title>First Post</title>
             </Head>
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">← Back to home</Link>
-            </h2>
+            <h1 className={styles.headingAlign}>First Post</h1>
         </Layout>
     );
 }
