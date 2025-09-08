5ed0cf7e02a3557e0c44176d25a6f190feb9595b
Eric-Cortez <ecrcortez@gmail.com>
2025-09-07 21:22:37 -0700

    feat: update home pg and post page style

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+
 
 export default function Home() {
   return (
@@ -9,17 +12,25 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>
+        <p className={`${styles.introText} ${styles.bgColor}`}>
           Hello, I'm Eric. I'm a software engineer with over 4 years of experience in the tech industry.
           I enjoy building dynamic, scalable applications that are intuitive and user-friendly.
           My passion lies in solving complex problems, learning new technologies, and collaborating with others to create impactful software solutions.
           In my free time, I like exploring new programming languages and contributing to open source projects.
         </p>
-        <p>
+        <p className={styles.secondaryText}>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <section>
+        <h2 className={styles.headingAlign}>Posts</h2>
+        <div className={styles.postCard}>
+          <Link href="/posts/first-post" className={styles.postLink}>First Post</Link>
+        </div>
+
+      </section>
+
     </Layout>
   );
 }
